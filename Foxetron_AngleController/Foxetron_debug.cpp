d5e361af3b0505286a1d149166699cb41bf32ca3
FancyFoxGems <tjbtech@yahoo.com>
2016-10-16 10:17:38 -0700

    asdf

---
+++ Foxetron_AngleController/Foxetron_debug.cpp
@@ -0,0 +1,5 @@
+/**********************************************************************************************
+* This file is part of the Foxetron program suite.                                            *
+* Copyright © 2016 Thomas J. Biuso III  ALL RIGHTS RESERVED...WHATEVER THAT MEANS.            *
+* RELEASED UNDER THE GPL v3.0 LICENSE; SEE <LICENSE> FILE WITHIN DISTRIBUTION ROOT FOR TERMS. *
+***********************************************************************************************/
