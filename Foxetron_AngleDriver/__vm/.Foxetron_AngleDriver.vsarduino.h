ede0f414924ffcf827b635890cd3f13edf56ec26
FancyFoxGems <tjbtech@yahoo.com>
2016-11-10 15:50:51 -0700

    asdf

--- Foxetron_AngleDriver/__vm/.Foxetron_AngleDriver.vsarduino.h
+++ Foxetron_AngleDriver/__vm/.Foxetron_AngleDriver.vsarduino.h
@@ -1,10 +1,10 @@
-/* 
+/*
 	Editor: http://www.visualmicro.com
 	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
 	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
-	
+
 	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
@@ -13,7 +13,7 @@
 #define __AVR_ATmega328p__
 #define __AVR_ATmega328P__
 #define _DEBUG 1
-#define EXCLUDE_ITTYBITTY_FULL_BYTES 1
+#define NO_ITTYBITTY_FULL_BYTES 1
 #define DEBUG_MESSAGES 1
 #define F_CPU 16000000L
 #define ARDUINO 10608
@@ -27,7 +27,7 @@
 #define __inline__
 #define __volatile__
 #define GCC_VERSION 40801
-#define volatile(va_arg) 
+#define volatile(va_arg)
 #define _CONST
 #define __builtin_va_start
 #define __builtin_va_end
@@ -48,7 +48,7 @@ extern "C" void __cxa_pure_virtual() {;}
 
 
 #include <arduino.h>
-#include <pins_arduino.h> 
+#include <pins_arduino.h>
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
